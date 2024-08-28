9c3aca3910c8bda438f5c1f6c93d9a178063fd3a
abdessamadsalim <samimalim1991@gmail.com>
2024-08-28 01:13:12 -0700

    create necessary file

---
+++ 0x00-personal_data/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+filter_datum = __import__('filtered_logger').filter_datum
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birt
+h=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=0
+3/04/1993;"]
+for message in messages:
+print(filter_datum(fields, 'xxx', message, ';'))
